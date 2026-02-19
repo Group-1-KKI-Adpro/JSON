3bf879471f67e4c4acddd8d0c666822fb0a05f87
alghani46 <alghani.gucci@gmail.com>
2026-02-19 15:07:41 +0700

    Initial commit

---
+++ src/main/java/id/ac/ui/cs/advprog/kki/json/JsonApplication.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.kki.json;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JsonApplication.class, args);
+    }
+
+}
