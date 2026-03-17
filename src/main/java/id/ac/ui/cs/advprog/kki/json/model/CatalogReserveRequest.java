50a372e315fa5866204f118ba94ffc056ac0aa68
EvelynOct <evelyne.aritonang72@gmail.com>
2026-03-17 18:52:37 +0700

    add reserve request dto for stock reservation

---
+++ src/main/java/id/ac/ui/cs/advprog/kki/json/model/CatalogReserveRequest.java
@@ -0,0 +1,14 @@
+package id.ac.ui.cs.advprog.kki.json.model;
+
+public class CatalogReserveRequest {
+
+    private int quantity;
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
