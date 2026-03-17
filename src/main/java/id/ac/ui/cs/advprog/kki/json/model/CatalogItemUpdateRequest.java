f76be179d6b0abea5709d8ea3d0c03c96d432c5f
EvelynOct <evelyne.aritonang72@gmail.com>
2026-03-17 18:50:53 +0700

    Add update request dto for catalog patch

---
+++ src/main/java/id/ac/ui/cs/advprog/kki/json/model/CatalogItemUpdateRequest.java
@@ -0,0 +1,32 @@
+package id.ac.ui.cs.advprog.kki.json.model;
+
+public class CatalogItemUpdateRequest {
+
+    private String description;
+    private Integer price;
+    private Integer stock;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
+    public Integer getStock() {
+        return stock;
+    }
+
+    public void setStock(Integer stock) {
+        this.stock = stock;
+    }
+}
