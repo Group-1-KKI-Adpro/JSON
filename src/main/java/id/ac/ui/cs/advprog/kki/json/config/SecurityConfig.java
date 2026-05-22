c538cd003cb81a0eaf3a8de016c68cbe45c3df8c
EvelynOct <evelyne.aritonang72@gmail.com>
2026-05-22 13:36:53 +0700

    fix: allow catalog add page route

--- src/main/java/id/ac/ui/cs/advprog/kki/json/config/SecurityConfig.java
+++ src/main/java/id/ac/ui/cs/advprog/kki/json/config/SecurityConfig.java
@@ -63,6 +63,7 @@ public class SecurityConfig {
                                 "/register",
                                 "/profile",
                                 "/catalog",
+                                "/catalog/new",
                                 "/orders",
                                 "/vouchers",
                                 "/wallet",
